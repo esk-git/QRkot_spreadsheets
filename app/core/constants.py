7374fabdccf2675a42ac2841fbc4366b4223fe65
Евгений Каликов <eskalikov@yandex.com>
2023-11-26 01:10:56 +0500

    Review 1

--- app/core/constants.py
+++ app/core/constants.py
@@ -2,3 +2,25 @@ LIFETIME_SECONDS_JWTSTRATEGY = int(3600)
 MAX_LENGHT = int(100)
 MIN_LENGHT = int(1)
 MIN_LENGHT_PASSWORD = int(3)
+LIST_TITLE = 'Лист1'
+ROWS = int(100)
+COLUMNS = int(11)
+SPREADSHEET_BODY = {
+    'properties': {'title': 'Отчёт',
+                   'locale': 'ru_RU'},
+    'sheets': [{'properties': {'sheetType': 'GRID',
+                               'sheetId': 0,
+                               'title': LIST_TITLE,
+                               'gridProperties': {'rowCount': ROWS,
+                                                  'columnCount': COLUMNS}}}]
+}
+SHEETS_NAME = 'sheets'
+SHEETS_VER = 'v4'
+
+DRIVE_VER = 'v3'
+TABLE_VALUES = [
+    ['Отчёт от', ],
+    ['Топ проектов по скорости закрытие'],
+    ['Название проекта', 'Время сбора', 'Описание']
+]
+FORMAT_TIME = "%Y/%m/%d %H:%M:%S"
--- app/services/google_api.py
+++ app/services/google_api.py
@@ -1,25 +1,23 @@
 from datetime import datetime
+from copy import deepcopy
 
 from aiogoogle import Aiogoogle
 from app.core.config import settings
-
-FORMAT = "%Y/%m/%d %H:%M:%S"
+from app.core.constants import (DRIVE_VER,
+                                FORMAT_TIME,
+                                SHEETS_NAME,
+                                SHEETS_VER,
+                                SPREADSHEET_BODY,
+                                TABLE_VALUES)
 
 
 async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
-    now_date_time = datetime.now().strftime(FORMAT)
-    service = await wrapper_services.discover('sheets', 'v4')
-    spreadsheet_body = {
-        'properties': {'title': f'Отчёт на {now_date_time}',
-                       'locale': 'ru_RU'},
-        'sheets': [{'properties': {'sheetType': 'GRID',
-                                   'sheetId': 0,
-                                   'title': 'Лист1',
-                                   'gridProperties': {'rowCount': 100,
-                                                      'columnCount': 11}}}]
-    }
+    spreadsheet_body_copy = deepcopy(SPREADSHEET_BODY)
+    now_date_time = datetime.now().strftime(FORMAT_TIME)
+    service = await wrapper_services.discover(SHEETS_NAME, SHEETS_VER)
+    spreadsheet_body_copy['properties']['title'] = f'Отчёт на {now_date_time}'
     response = await wrapper_services.as_service_account(
-        service.spreadsheets.create(json=spreadsheet_body)
+        service.spreadsheets.create(json=spreadsheet_body_copy)
     )
     spreadsheetid = response['spreadsheetId']
     return spreadsheetid
@@ -32,7 +30,7 @@ async def set_user_permissions(
     permissions_body = {'type': 'user',
                         'role': 'writer',
                         'emailAddress': settings.email}
-    service = await wrapper_services.discover('drive', 'v3')
+    service = await wrapper_services.discover('drive', DRIVE_VER)
     await wrapper_services.as_service_account(
         service.permissions.create(
             fileId=spreadsheetid,
@@ -46,13 +44,11 @@ async def spreadsheets_update_value(
         charity_projects: list,
         wrapper_services: Aiogoogle
 ) -> None:
-    now_date_time = datetime.now().strftime(FORMAT)
-    service = await wrapper_services.discover('sheets', 'v4')
-    table_values = [
-        ['Отчёт от', now_date_time],
-        ['Топ проектов по скорости закрытие'],
-        ['Название проекта', 'Время сбора', 'Описание']
-    ]
+    now_date_time = datetime.now().strftime(FORMAT_TIME)
+    service = await wrapper_services.discover(SHEETS_NAME, SHEETS_VER)
+
+    table_values = deepcopy(TABLE_VALUES)
+    table_values[0].append(now_date_time)
     for project in charity_projects:
         new_row = [
             project.name,
